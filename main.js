5f5d24c0008260c0a19aca702f16bcde8410303c
Char B <charburton18@gmail.com>
2024-03-18 17:24:53 -0700

    Added arrays together using setInterval

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-
-
+// FREELANCER FORUM WORKSHOP ----------------------------------------------------------------------
 
 // A visitor enters the website and finds a list of available freelancers. Each freelancer has a name, an occupation, and a starting price for their services. They observe on the list Alice, the writer with a starting price of $30, and Bob, who is a teacher, has a starting price of $50.
 
@@ -9,93 +8,117 @@
 
 // New freelancers continue to appear every few seconds, and the average starting price is updated accordingly.
 
+// They observe on the list Alice, the writer with a starting price of $30, and Bob, who is a teacher, has a starting price of $50.
 
-
-
+// My pseudocode:
 // connected HTML and JS and CSS files
 // Made basic HTML file with h1, h2, h3
 // initialize an array of at least two freelancers with names, occupations, and starting prices.
-
-
 
 /* State */
 // Here, we define variables for the data that our program needs to remember.
 // We call this data "state" because it represents the state of our program.
 
-// They observe on the list Alice, the writer with a starting price of $30, and Bob, who is a teacher, has a starting price of $50.
-const originalFreelanceArr = [
+
+// DEFINE YOUR ARRAYS -----------------------------------------------------------------------------
+
+// Define original array
+const fArr1 = [
 {name: 'Alice',
 occupation: 'Writer',
-startingPrice: 30
+price: 30
 },
 {name: 'Bob',
 occupation: 'Teacher',
-startingPrice: 50
+price: 50
 },
 ];
 
-const newFreelancerArr = [
-    { name: "Dr. Slice", price: 25, occupation: "gardener" },
-    { name: "Dr. Pressure", price: 51, occupation: "programmer" },
-    { name: "Prof. Possibility", price: 43, occupation: "teacher" },
-    { name: "Prof. Prism", price: 81, occupation: "teacher" },
-    { name: "Dr. Impulse", price: 43, occupation: "teacher" },
-    { name: "Prof. Spark", price: 76, occupation: "programmer" },
-    { name: "Dr. Wire", price: 47, occupation: "teacher" },
-    { name: "Prof. Goose", price: 72, occupation: "driver" },
+// Define new array. Each of these objects will show on the webpage every 3 seconds.
+const fArr2 = [
+    {name: 'Carol', occupation: 'Programmer', price: 70},
+    {name: 'Dr. Slice', occupation: 'gardener', price: 25},
+    { name: 'Dr. Pressure', occupation: 'programmer', price: 51},
+    { name: 'Prof. Possibility', occupation: 'teacher', price: 43},
+    { name: 'Prof. Prism', occupation: 'teacher', price: 81},
+    { name: 'Dr. Impulse', occupation: 'teacher', price: 43},
+    { name: 'Prof. Spark', occupation: 'programmer', price: 76},
+    { name: 'Dr. Wire', occupation: 'teacher', price: 47},
+    { name: 'Prof. Goose', occupation: 'driver', price: 72},
   ];
 
-//render(); // We call this function once to render the initial state
 
-/**
- * Update the DOM to reflect the current state.
- * The term "render" is often used to describe this process.
-*/
-
-// GET THE AVERAGE PRICE OF THE CURRENT ARRAY
+// GET THE AVERAGE PRICE OF THE CURRENT ARRAY -----------------------------------------------------
 
 // make a function that finds the average price of all of the objects inside an array. 
   // loop through each object using the array Method reduce()
-    // define a callback function inside reduce() called getSum that adds all the obj prices together
+    // define a callback function inside reduce() called getSum that sums all the obj prices
     const getSum = (total, currentObj) => {
-      return total + currentObj.startingPrice;
+      return total + currentObj.price;
     }
     // define another function called getAverage that invokes the getSum function and divides its output by number of array elements (array.length)
     const getAverage = (arr) => {
-      return originalFreelanceArr.reduce(getSum, 0)/(arr.length);
+      return fArr1.reduce(getSum, 0)/(arr.length);
     }
     // call our getAverage function, define it as a variable
-const arrAverage = getAverage(originalFreelanceArr); // returns the average startingPrice of the array passed in
+const arrAverage = getAverage(fArr1); // returns the average startingPrice of the array passed in
 
 
-//we need to put this arrAverage into the HTML... hmmm
+// DISPLAY THE AVERAGE PRICE ON WEBPAGE -----------------------------------------------------------
+
+//we need to put this arrAverage into the HTML...
     // I added an h3 with the id "average"
-    // lets grab this h3
-
+    // let's grab this h3 with querySelector
 const averageSelectedh3 = document.querySelector('#average');
 
-averageSelectedh3.innerText = `The average starting price is $${arrAverage}` // this puts our array's average startingPrice into the HTML :)
+// this puts our array's average startingPrice into the HTML
+averageSelectedh3.innerText = `The average starting price is $${arrAverage}` 
 
-// Help from Anusha:
-// How to put the array in the HTML?
-// what if every 3 seconds I grab the first element of arr2 and push onto arr1?
+
+// ADD fArr2 TO THE END OF fArr1 EVERY 3 SECONDS ------------------------------------------------------------------
+
+// What if every 3 seconds I grab the first element of arr2 and push onto arr1?
 // pushArrFunc takes in 2 arrays
+// create variable const firstItem = arr 2's first element that gets spliced, sliced, or shifted 
+// firstItem gets pushed into the end of the new array (push returns array length)
+// return arr1
 const pushArrFunc = (arr1, arr2) => {
-  // slice 0th index of arr2
-  // push 0th index of arr2 into arr1
-  // loop through arr2 and take out the arr elements in order. 
-    // splice out the currentArrElement and place it into arr1
-  for(let i = 0; i < newFreelancerArr.length; i++) {
-  const splicedFreelancerArr = arr2.splice(i);
-  setInterval(arr1.push(splicedFreelancerArr[i]), 3000);
-  }
+  const firstItem = arr2.shift();
+  arr1.push(firstItem);
+  console.log(arr1);
   return arr1;
 }
-console.log(pushArrFunc(originalFreelanceArr, newFreelancerArr));
-//define pushEvery3Sec
-// console.log(setInterval(pushArrFunc(originalFreelanceArr, newFreelancerArr), 3000));
-//console.log(pushEvery3Sec(pushArrFunc(originalFreelanceArr, newFreelancerArr)));
+setInterval(() => {pushArrFunc(fArr1, fArr2)}, 3000);
 
+// IF I WANTED TO STOP THE SET INTERVAL -------------------------------------
+// setInterval(() => {pushArrFunc(fArr1, fArr2)}, 3000);
+//when arr2 = [] then clearInterval();
+// const returnID = setInterval(() => {pushArrFunc(fArr1, fArr2)}, 3000);
+// if(fArr2.length === 0) {
+//   clearInterval(returnID);
+// }
+
+// DISPLAY fArr1 ON THE PAGE -------------------------------------------------
+
+
+
+//   // slice 0th index of arr2
+//   // push 0th index of arr2 into arr1
+//   // loop through arr2 and take out the arr elements in order. 
+//     // splice out the currentArrElement and place it into arr1
+//   for(let i = 0; i < fArr2.length; i++) {
+//   const splicedFreelancerArr = arr2.splice(i);
+//   setInterval(arr1.push(splicedFreelancerArr[i]), 3000);
+//   }
+//   return arr1;
+// }
+// console.log(pushArrFunc(fArr1, fArr2));
+//define pushEvery3Sec
+// console.log(setInterval(pushArrFunc(fArr1, fArr2), 3000));
+//console.log(pushEvery3Sec(pushArrFunc(fArr1, fArr2)));
+
+
+// How to put the array in the HTML?
 // use querySelector to grab the ul
 // First create li's with the document.createElement("li") in the DOM
 // use replaceChildren() to replace the ul's (#ulFreelancerList) children with the keyvalues of each object
@@ -112,7 +135,7 @@ console.log(pushArrFunc(originalFreelanceArr, newFreelancerArr));
 
   
 
-// I have to push the objects from freelancers array into the existing originalFreelanceArr every 3 seconds
+// I have to push the objects from freelancers array into the existing fArr1 every 3 seconds
 // I will be using setInterval() and push()
 // setInterval will call the push() function every 3000 milliseconds 
 
@@ -120,4 +143,12 @@ console.log(pushArrFunc(originalFreelanceArr, newFreelancerArr));
 // I think simply adding objects to the original array with the above mentioned methods will do that.. IF I somehow put the array onto the page
 // can an array be sent to HTML as a list? How can an array be listed on a website in an understandable way?
 
+
+//Other code:
+//render(); // We call this function once to render the initial state
+
+/**
+ * Update the DOM to reflect the current state.
+ * The term "render" is often used to describe this process.
+*/
   
